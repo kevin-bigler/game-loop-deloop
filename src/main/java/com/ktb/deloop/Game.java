094efd25208bd923b5e913920262f09fff25ce55
Kevin Bigler <kevinbigler@gmail.com>
2017-10-08 18:56:35 +0100

    Add project structure. Add start of a Game class with a game loop in it.

---
+++ src/main/java/com/ktb/deloop/Game.java
@@ -0,0 +1,59 @@
+package com.ktb.deloop;
+
+public class Game implements Runnable {
+    private Thread thread;
+    private boolean running = false;
+    private static final double MAX_DT = 0.15;
+
+    @Override
+    public void run() {
+        running = true;
+        double lastTime = currentTime();
+        double time = 0;
+        double dt = 0;
+
+        while (running) {
+            time = currentTime();
+            dt = lastTime - time;
+            lastTime = time;
+
+            if (dt > MAX_DT) {
+                dt = MAX_DT;
+            }
+
+            getInput();
+            update(dt);
+            draw();
+        }
+    }
+
+    private void getInput() {
+
+    }
+
+    private void update(double dt) {
+
+    }
+
+    private void draw() {
+
+    }
+
+    private double currentTime() {
+        return System.nanoTime() / Double.valueOf(10^9);
+    }
+
+    public void start() {
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public void stop() {
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/ktb/deloop/Main.java
@@ -0,0 +1,7 @@
+package com.ktb.deloop;
+
+public class Main {
+    public static void main(final String args[]) {
+        System.out.println("doing stuff");
+    }
+}
