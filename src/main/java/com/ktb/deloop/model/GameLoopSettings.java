93630d5cf2082d5a841b44aec68aa284af12a37f
Kevin Bigler <kevinbigler@gmail.com>
2017-11-24 20:08:51 +0000

    More TODO notes

--- src/main/java/com/ktb/deloop/model/GameLoopSettings.java
+++ src/main/java/com/ktb/deloop/model/GameLoopSettings.java
@@ -9,6 +9,7 @@ import lombok.Data;
 @Data
 @Builder
 public class GameLoopSettings {
+    // TODO change settings to be in terms of min FPS, max FPS (then these values extrapolated from that)
     private double maxDt;
     private double minDt;
     private double fpsUpdateFreq;
