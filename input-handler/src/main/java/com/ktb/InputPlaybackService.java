7450f5e5f58d71199068c19174b7118c47c8908e
Kevin Bigler <kevinbigler@gmail.com>
2018-03-02 21:33:24 +0000

    input handler impl stubs

--- input-handler/src/main/java/com/ktb/InputPlaybackService.java
+++
@@ -1,5 +0,0 @@
-package com.ktb;
-
-// TODO: accept stream of input events and spout them off
-public class InputPlaybackService {
-}
---
+++ input-handler/src/main/java/com/ktb/PlaybackInputHandler.java
@@ -0,0 +1,19 @@
+package com.ktb;
+
+// TODO: accept stream of input events and spout them off
+public class PlaybackInputHandler implements InputHandler {
+    @Override
+    public void subscribe(InputEvent event, Subscriber<? extends InputEvent, ? extends InputEventData> subscriber) {
+
+    }
+
+    @Override
+    public void unsubscribe(InputEvent event, Subscriber<? extends InputEvent, ? extends InputEventData> subscriber) {
+
+    }
+
+    @Override
+    public void dispatch(InputEvent event, InputEventData data) {
+
+    }
+}
